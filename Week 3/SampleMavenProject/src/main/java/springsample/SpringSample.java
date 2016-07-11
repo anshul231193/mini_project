b3a5edbfa1d35c0d3a8b1cee016bd6fee267cf14
Anshul Gupta <anshulgupta231193@gmail.com>
2016-07-11 17:33:06 +0530

    Maven Spring Project.

---
+++ Week 3/SampleMavenProject/src/main/java/springsample/SpringSample.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package springsample;
+
+import com.sample.service.CustomerService;
+import com.sample.service.CustomerServiceImpl;
+
+/**
+ *
+ * @author anshul
+ */
+public class SpringSample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CustomerService service = new CustomerServiceImpl();
+        
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+    
+}
